55ebe8bd26acf5300fd96ac37c94aa77e8d946db
nhhoang188 <72209266+nhhoang188@users.noreply.github.com>
2020-10-21 12:19:22 +0700

    a

--- Rectangle/creat-Rectangle.js
+++ Rectangle/creat-Rectangle.js
@@ -2,25 +2,27 @@ class Rectangle {
     constructor(width, height) {
         this.height = height;
         this.width = width;
-        this.perimeter = 2 * (this.height + this.width);
-        this.s = this.height * this.width;
         let ctx = document.getElementById('myCanvas').getContext('2d');
         ctx.beginPath();
         ctx.strokeStyle = "red";
         ctx.rect(10, 10, this.width, this.height);
         ctx.stroke();
-    this.setHeight = function (height) {
+    }
+    setHeight(height){
        return this.height = height;
     }
-    this.setWidth = function (width) {
+    setWidth(width){
        return this.width = width;
     }
+    getP() {
+        return 2 * (this.height + this.width);
+    }
+    getS(){
+        return this.height * this.width;
+    }
 
-}
 }
 
 let chunhat = new Rectangle(500, 400);
-chunhat.setHeight(100)
-chunhat.setWidth(300);
-console.log(chunhat.perimeter);
-console.log(chunhat.s);
+console.log(chunhat.getP());
+console.log(chunhat.getS());
